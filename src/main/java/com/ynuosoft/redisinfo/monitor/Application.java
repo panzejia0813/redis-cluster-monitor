2b8264a9eac118fdb0bb3dec099b5c2f8dd26bb8
Pan Zejia <164390723@qq.com>
2019-12-23 23:42:37 +0800

    提交

---
+++ src/main/java/com/ynuosoft/redisinfo/monitor/Application.java
@@ -0,0 +1,25 @@
+package com.ynuosoft.redisinfo.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by Pan Zejia on 2017/1/16.
+ */
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setRegisterShutdownHook(false);
+        springApplication.setWebEnvironment(false);
+        //
+        //springApplication.setAddCommandLineProperties(false);
+        springApplication.run(args);
+
+    }
+
+}
---
+++ src/main/java/com/ynuosoft/redisinfo/monitor/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.ynuosoft.redisinfo.monitor.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Pan Zejia on 2017/2/24.
+ */
+@Configuration
+public class AppConfig {
+
+}
